c28f4315e9ce73bf8823203ee2879a66a47e0ba4
danil <d.prytckov@yandex.ru>
2020-04-01 20:36:48 +0500

    install gulp and sass

---
+++ public_html/gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    console.log("Компиляция SASS");
+    gulp.src('./public/css/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./public/css/*.sass', gulp.series('sass'));
+});
