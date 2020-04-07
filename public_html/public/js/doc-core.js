7b05deed2b863eebbb16200b8a9d5a0ce9266ee8
danil <d.prytckov@yandex.ru>
2020-04-08 02:47:57 +0500

    Документация почти составленна

--- public_html/public/js/doc-core.js
+++ public_html/public/js/doc-core.js
@@ -1,10 +1,16 @@
-function load_theme(path, name) {
-    frame.attr("src", path);
-    frame.css("display", "none").fadeIn(1000);
-    name_lesson.text("«" + name + "»");
+function load_theme(path, name, height = 3000) {
+    if (tmout){
+        tmout = false;
+        frame.attr("src", path);
+        frame.css("height", height);
+        frame.css("display", "none").fadeIn(300);
+        name_lesson.text("«" + name + "»");
+        setTimeout(function () {tmout = true}, 100);
+    }
 }
 
 const frame = $("#frame");
 const name_lesson = $("#name-lesson");
+let tmout = true;
 
-load_theme("documents/section_1/theme_1/index.html", "Введение. Основы сетей");
+load_theme("documents/section_1/theme_1/index.html", "Введение", 3000);
---
+++ public_html/public/js/frame-auto-height.js
@@ -0,0 +1,4 @@
+console.log("123");
+
+var iframe = $('#frame', parent.document.body);
+iframe.height($(document.body).height() + 100);
